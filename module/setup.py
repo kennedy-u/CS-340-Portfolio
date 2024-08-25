3dbcc51885c6a793e8a7ea915ce95006e511b1c0
kennedy <kennedyuzoho@outlook.com>
2024-08-25 02:07:01 -0400

    update project files and docs

---
+++ module/setup.py
@@ -0,0 +1,12 @@
+"""Setup file"""
+from setuptools import setup
+
+setup(
+    name='animalshelter',
+    version='1.0.0',
+    description='A package to interact with a MongoDB database',
+    author='Kennedy Uzoho',
+    author_email='kennedy.uzoho@snhu.edu',
+    url='xxxxxxxxxxxx',
+    py_modules=['animal_shelter'],
+)
